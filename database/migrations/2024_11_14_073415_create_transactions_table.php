b0df7e3a568f5d84b7bd5a4f08fb962d54af7604
newsa720 <rda0044@gmail.com>
2024-11-14 11:43:59 +0330

    add softdeletes

--- database/migrations/2024_11_14_073415_create_transactions_table.php
+++ database/migrations/2024_11_14_073415_create_transactions_table.php
@@ -5,8 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -18,6 +17,7 @@ return new class extends Migration
             $table->string('description');
             $table->foreignIdFor(Category::class);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
