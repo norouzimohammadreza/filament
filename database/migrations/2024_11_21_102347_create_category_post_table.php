28ba36e629a02b806f04e6e9fa3944305c068773
newsa720 <rda0044@gmail.com>
2024-11-21 13:56:51 +0330

    add category_post migration

---
+++ database/migrations/2024_11_21_102347_create_category_post_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('category_post', function (Blueprint $table) {
+            $table->unsignedBigInteger('category_id')->index();
+            $table->unsignedBigInteger('post_id')->index();
+            $table->primary(['category_id', 'post_id']);
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('category_post');
+    }
+};
