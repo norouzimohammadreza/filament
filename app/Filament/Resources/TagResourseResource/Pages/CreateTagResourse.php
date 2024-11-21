04fdb06cc9bc8c6f0e1fcd45dbc73b32bf91f884
newsa720 <rda0044@gmail.com>
2024-11-21 12:28:48 +0330

    show tags

--- app/Filament/Resources/TagResourseResource/Pages/CreateTagResourse.php
+++ app/Filament/Resources/TagResourseResource/Pages/CreateTagResourse.php
@@ -9,4 +9,8 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateTagResourse extends CreateRecord
 {
     protected static string $resource = TagResourseResource::class;
+    protected function getRedirectUrl() :string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
