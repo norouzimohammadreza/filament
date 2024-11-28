704b2f68d93dcafeada9e9944f3e3c5385c6e2cc
newsa720 <rda0044@gmail.com>
2024-11-28 12:06:33 +0330

    create a new migration for activity log and add log level field to the table

---
+++ database/migrations/2024_11_28_083123_add_level_to_activity_log.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Enums\LogLevelEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('activity_log', function (Blueprint $table) {
+            $table->tinyInteger('level')
+                ->default(LogLevelEnum::Low->value)->after('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('activity_log', function (Blueprint $table) {
+            //
+        });
+    }
+};
