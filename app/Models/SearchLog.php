df14f4215a1f9080e0556727b41a5b6fad40db45
newsa720 <rda0044@gmail.com>
2024-12-01 23:48:36 +0330

    edit migration and model

--- app/Models/SearchLog.php
+++ app/Models/SearchLog.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class SearchLog extends Model
 {
-    //
+    protected $fillable = [
+        'resource',
+        'search_query',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2024_12_01_200637_create_search_logs_table.php
+++ database/migrations/2024_12_01_200637_create_search_logs_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,8 +14,15 @@ return new class extends Migration
     {
         Schema::create('search_logs', function (Blueprint $table) {
             $table->id();
+            $table->string('resource');
+            $table->string('search_query');
+            $table->foreignIdFor(User::class)->index();
             $table->timestamps();
         });
+        $this->iplog();
+    }
+    public function iplog(): void{
+        DB::statement('ALTER TABLE `search_logs` ADD `ip` VARBINARY(16) AFTER `id`');
     }
 
     /**
