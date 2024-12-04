00ff48d4dbca70ec09e87fb18fa10c3e952b6420
newsa720 <rda0044@gmail.com>
2024-12-04 11:40:30 +0330

    edit model of logging info

--- app/Models/LoggingInfo.php
+++ app/Models/LoggingInfo.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class LoggingInfo extends Model
 {
-    //
+    protected $fillable = [
+        'model_id',
+        'model_type',
+        'details',
+        'level'
+    ];
 }
