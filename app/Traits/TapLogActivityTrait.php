a876f779bea906a3b322775f20df11887c58901d
newsa720 <rda0044@gmail.com>
2024-12-09 12:09:33 +0330

    Adding a trait for customizing logs activities

---
+++ app/Traits/TapLogActivityTrait.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Traits;
+
+use App\Enums\LogDetailsAsModelEnum;
+use Spatie\Activitylog\Models\Activity;
+
+trait TapLogActivityTrait
+{
+    public function tapActivity(Activity $activity, string $eventName)
+    {
+        $this->checkIfLoggingIsEnabled();
+        if ($this->specificallyModel() != null) {
+            if ($this->specificallyModel()->details == LogDetailsAsModelEnum::ENABLED->value
+                && $this->logLevel >= $this->specificallyModel()->level) {
+                activity()->enableLogging();
+                $activity->level = $this->specificallyModel()->level;
+            } else {
+                activity()->disableLogging();
+            }
+        } else {
+            $activity->level = $this->logLevel;
+        }
+        $activity->ip = inet_pton(request()->ip());
+        $activity->url = request()->getPathInfo();
+
+    }
+}
