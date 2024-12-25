876adab22b7f3122573ffabb3cba85d9e32ca122
newsa720 <rda0044@gmail.com>
2024-12-25 11:54:50 +0330

    show models activity

--- app/Filament/Pages/ModelActivity.php
+++ app/Filament/Pages/ModelActivity.php
@@ -2,9 +2,14 @@
 
 namespace App\Filament\Pages;
 
+use App\Enums\LogLevelEnum;
 use App\Models\ModelLog;
+use Filament\Forms\Components\Toggle;
 use Filament\Pages\Page;
+use Filament\Tables\Actions\SelectAction;
+use Filament\Tables\Columns\SelectColumn;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ToggleColumn;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Table;
@@ -22,8 +27,17 @@ class ModelActivity extends Page implements HasTable
         return $table
             ->query(ModelLog::query())
             ->columns([
-                TextColumn::make('model_type')
-                    ->getStateUsing(fn() => config('farda_activity_log.models'))
+                TextColumn::make('model_type')->label('Model')
+                    ->getStateUsing
+                    (fn(ModelLog $record) => (explode('\\', $record->model_type)[2])),
+                ToggleColumn::make('is_enabled')->label('Enabled'),
+                SelectColumn::make('logging_level')->label('Level')
+                    ->options([
+                        LogLevelEnum::LOW->value => 'Low',
+                        LogLevelEnum::MEDIUM->value => 'Medium',
+                        LogLevelEnum::HIGH->value => 'High',
+                        LogLevelEnum::CRITICAL->value => 'Critical',
+                    ])
             ])
             ->filters([])
             ->actions([])
--- resources/views/filament/pages/model-activity.blade.php
+++ resources/views/filament/pages/model-activity.blade.php
@@ -10,36 +10,5 @@
         //dd(\App\Enums\LogLevelEnum::value(1));
         $levels = (\App\Enums\LogLevelEnum::values());
     @endphp
-    <table>
-        <thead>
-            <td>state</td>
-            <td>Level</td>
-            <td>Is_Enabled</td>
-        </thead>
-        <tr>
-            <td>Activity Log</td>
-            <td>{{\App\Enums\LogLevelEnum::value(\App\ActivityLogsFunctions\ActivityLogHelper::$MINIMUM_LOGGING_LEVEL)}}</td>
-            <td>{{\App\Enums\LogDetailsAsModelEnum::value(\App\ActivityLogsFunctions\ActivityLogHelper::$LOGGING_ENABLED)}}</td>
-
-        </tr>
-    </table>
-    <hr>
-    <table>
-        <thead>
-        <td>Model</td>
-        <td>Level</td>
-        <td>Enabled</td>
-        </thead>
-        <tbody>
-        @foreach($logModels as $logModel)
-            <tr>
-
-                <td>{{$logModel->model_type}}</td>
-                <td>{{\App\Enums\LogLevelEnum::value($logModel->logging_level)}}</td>
-                <td>{{$logModel->is_enabled?'True':'False'}}</td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-
+{{$this->table}}
 </x-filament-panels::page>
