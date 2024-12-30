f2abde1a840d0119a39e6f167b73a8ebab7b69a5
newsa720 <rda0044@gmail.com>
2024-12-30 12:18:32 +0330

    Fix log for a instance of model when it is off

--- app/ActivityLogsFunctions/Traits/MyLogActivityTrait.php
+++ app/ActivityLogsFunctions/Traits/MyLogActivityTrait.php
@@ -13,11 +13,8 @@ use Spatie\Activitylog\Models\Activity;
 trait MyLogActivityTrait
 {
     public int $logLevel = LogLevelEnum::LOW->value;
-    public function tapActivity(Activity $activity, string $eventName, int $event_log_level = LogLevelEnum::LOW->value)
+    public function setLevelByEvent(string $eventName) : int
     {
-        $activity->ip = inet_pton(request()->ip());
-        $activity->url = request()->getPathInfo();
-
         //TODO logging based on event type!?
         switch ($eventName) {
             case 'created' :
@@ -30,28 +27,20 @@ trait MyLogActivityTrait
                 $event_log_level = LogLevelEnum::CRITICAL->value;
                 break;
         }
+        return $event_log_level;
+    }
+    public function tapActivity(Activity $activity, string $eventName, int $event_log_level = LogLevelEnum::LOW->value)
+    {
+        $activity->ip = inet_pton(request()->ip());
+        $activity->url = request()->getPathInfo();
+        $event_log_level = $this->setLevelByEvent($eventName);
+
 
         if (!ActivityLogHelper::getInstance()->getAppLoggingIsEnabled()) {
             activity()->disableLogging();
             return;
         }
 
-        if ($this->modelRecordLogSettings != null) {
-            if (!$this->modelRecordLogSettings->is_enabled || $event_log_level < $this->modelRecordLogSettings->logging_level) {
-                activity()->disableLogging();
-                return;
-            }
-            $this->enableLogging();
-            activity()->enableLogging();
-            $activity->level = $event_log_level;
-            return;
-        }
-
-        //TODO where set?
-        if (!$this->enableLoggingModelsEvents) {
-            activity()->disableLogging();
-            return;
-        }
 
         if ($this->modelLogSetting()->follow_global_config == 1) {
             if ($event_log_level < ActivityLogHelper::getInstance()->getAppMinimumLoggingLevel()) {
@@ -75,10 +64,26 @@ trait MyLogActivityTrait
     {
         //TODO
         $logStatus = app(ActivityLogStatus::class);
+        $event_log_level = $this->setLevelByEvent($eventName);
+        if ($this->modelRecordLogSettings != null) {
+            if (!$this->modelRecordLogSettings->is_enabled || $event_log_level < $this->modelRecordLogSettings->logging_level) {
+                activity()->disableLogging();
+                return false;
+            }
+            $this->enableLogging();
+            activity()->enableLogging();
+            \activity()->tap(function (Activity $activity) use ($event_log_level) {
+               $activity->level = $event_log_level;
+            });
+            return true;
+        }
 
-        if (! $this->enableLoggingModelsEvents || $logStatus->disabled()) {
+        //TODO where set?
+        if (!$this->enableLoggingModelsEvents) {
+            activity()->disableLogging();
             return false;
         }
+
 
         if (! in_array($eventName, ['created', 'updated'])) {
             return true;
