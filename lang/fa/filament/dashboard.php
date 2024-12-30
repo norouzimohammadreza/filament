8fc083a87dc4db6d7ec2497240e7c75fff8ce2fa
newsa720 <rda0044@gmail.com>
2024-12-30 15:36:53 +0330

    add lang for persian

---
+++ lang/fa/filament/dashboard.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    'dashboard' => 'داشبورد',
+    'model_log_setting' => 'تنظیمات لاگ مدل',
+    'categories' => 'دسته‌ها',
+    'activities' => 'فعالیت‌ها',
+    'model_instance_log_settings' => 'تنظیمات لاگ نمونه مدل',
+    'posts' => 'پست‌ها',
+    'tags' => 'برچسب‌ها',
+    'transactions' => 'تراکنش‌ها',
+    'users' => 'کاربران',
+
+];
+
