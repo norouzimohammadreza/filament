cfbfa5b7d7be8cd0535411ab383a65331012560e
newsa720 <rda0044@gmail.com>
2024-12-30 15:37:14 +0330

    add lang dashboard for english

---
+++ lang/en/filament/dashboard.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'dashboard' => 'Dashboard',
+    'model_log_setting' => 'Model Log Setting',
+    'categories' => 'Categories',
+    'activities' => 'Activities',
+    'model_instance_log_settings' => 'Model Instance Log Settings',
+    'posts' => 'Posts',
+    'tags' => 'Tags',
+    'transactions' => 'Transactions',
+    'users' => 'Users',
+
+];
