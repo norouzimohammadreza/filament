7577f999b9776ba26bc9d6907532050889c6d05b
newsa720 <rda0044@gmail.com>
2024-12-30 16:25:06 +0330

    translate model activity pages

---
+++ lang/en/filament/model_activity.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'log_setting' => 'Log Setting',
+    'app' => 'Application',
+    'global_log_setting' => 'Global Log Setting',
+    'enabled' => 'Enabled',
+    'level' => 'Level',
+    'models' => 'Models',
+    'follow_global_config' => 'Follow Global Config',
+
+];
---
+++ lang/fa/filament/model_activity.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'log_setting' => 'تنظیمات لاگ',
+    'app' => 'برنامه',
+    'global_log_setting' => 'تنظیمات لاگ سراسری',
+    'enabled' => 'فعال',
+    'level' => 'سطح',
+    'models' => 'مدل‌ها',
+    'follow_global_config' => 'پیروی از تنظیمات سراسری',
+
+];
