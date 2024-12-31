8ec5c37ae9e8d9636bfb4ff391ca18ff2ee73a75
norouzimohammadreza <rda0044@gmail.com>
2024-12-31 10:42:02 +0330

    Edit category translate files for delete and create msg

--- lang/en/filament/category.php
+++ lang/en/filament/category.php
@@ -2,6 +2,8 @@
 return [
     'name' => 'Name',
     'new_category' => 'New Category',
+    'create_category' => 'Create Category',
     'edit_category' => 'Edit Category',
+    'delete_category' => 'Delete Category',
 
 ];
--- lang/fa/filament/category.php
+++ lang/fa/filament/category.php
@@ -2,5 +2,7 @@
 return [
     'name' => 'نام',
     'new_category' => 'دسته‌بندی جدید',
+    'create_category' => 'ایجاد دسته‌بندی',
     'edit_category' => 'ویرایش دسته‌بندی',
+    'delete_category' => 'حذف دسته‌بندی',
 ];
