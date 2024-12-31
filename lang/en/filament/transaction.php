ddbe072ccc74e741f7c1f889b6b91fd5dce73456
norouzimohammadreza <rda0044@gmail.com>
2024-12-31 13:24:24 +0330

    Adding translate files for transactions

---
+++ lang/en/filament/transaction.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'create_transaction' => 'Create Transaction',
+    'edit_transaction' => 'Edit Transaction',
+    'delete_transaction' => 'Delete Transaction',
+    'new_transaction' => 'New Transaction',
+    'amount' => 'Amount',
+    'description' => 'Description',
+    'category' => 'Category',
+];
---
+++ lang/fa/filament/transaction.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'create_transaction' => 'ایجاد تراکنش',
+    'edit_transaction' => 'ویرایش تراکنش',
+    'delete_transaction' => 'حذف تراکنش',
+    'new_transaction' => 'تراکنش جدید',
+    'amount' => 'مقدار',
+    'description' => 'توضیحات',
+    'category' => 'دسته‌بندی',
+];
+
