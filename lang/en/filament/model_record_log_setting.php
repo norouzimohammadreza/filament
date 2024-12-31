b2ddcf4a8dd79ffc3df640d8f6a230998e49a44d
norouzimohammadreza <rda0044@gmail.com>
2024-12-31 10:41:36 +0330

    Adding translate files for model record log settings

---
+++ lang/en/filament/model_record_log_setting.php
@@ -0,0 +1,12 @@
+<?php
+return[
+    'model_record_log_setting' => 'Model Record Log Setting',
+    'model_type' => 'Model Type',
+    'model_id' => 'Model ID',
+    'level' => 'Level',
+    'enabled' => 'Enabled',
+    'new_model_record' => 'New Model Record',
+    'edit_model_record' => 'Edit Model Record',
+    'create_model_record' => 'Create Model Record',
+    'delete_model_record' => 'Delete Model Record',
+];
---
+++ lang/fa/filament/model_record_log_setting.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'model_record_log_setting' => 'تنظیمات ثبت لاگ رکورد مدل',
+    'model_type' => 'نوع مدل',
+    'model_id' => 'شناسه مدل',
+    'level' => 'سطح',
+    'enabled' => 'فعال',
+    'new_model_record' => 'رکورد جدید مدل',
+    'edit_model_record' => 'ویرایش رکورد مدل',
+    'create_model_record' => 'ایجاد رکورد مدل',
+    'delete_model_record' => 'حذف رکورد مدل',
+];
