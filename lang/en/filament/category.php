e9d5d63b3550feac2c2989295168379d423c3775
newsa720 <rda0044@gmail.com>
2024-12-31 09:35:02 +0330

    translate files for category pages

---
+++ lang/en/filament/category.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'name' => 'Name',
+    'new_category' => 'New Category',
+    'edit_category' => 'Edit Category',
+
+];
---
+++ lang/fa/filament/category.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'name' => 'نام',
+    'new_category' => 'دسته‌بندی جدید',
+    'edit_category' => 'ویرایش دسته‌بندی',
+];
