ca597f05ca4d4ae693e23d0b3db88eac240c8692
norouzimohammadreza <rda0044@gmail.com>
2024-12-31 13:41:51 +0330

    Adding translate files for users

---
+++ lang/en/filament/user.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'create_user' => 'Create User',
+    'edit_user' => 'Edit User',
+    'delete_user' => 'Delete User',
+    'new_user' => 'New User',
+    'name' => 'Name',
+    'email' => 'Email',
+    'role' => 'Role',
+    'change_password' => 'Change Password',
+    'password' => 'Password',
+    'confirm_password' => 'Confirm Password',
+
+];
---
+++ lang/fa/filament/user.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'create_user' => 'ایجاد کاربر',
+    'edit_user' => 'ویرایش کاربر',
+    'delete_user' => 'حذف کاربر',
+    'new_user' => 'کاربر جدید',
+    'name' => 'نام',
+    'email' => 'ایمیل',
+    'role' => 'نقش',
+    'change_password' => 'تغییر رمز عبور',
+    'password' => 'رمز عبور',
+    'confirm_password' => 'تأیید رمز عبور',
+];
+
