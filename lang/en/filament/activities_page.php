585b748ec10db8b80994c9cf8592b2e33e26a6e6
Norouzimohammadreza <rda0044@gmail.com>
2024-12-31 09:44:58 +0330

    Translates file for activities file

---
+++ lang/en/filament/activities_page.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'ip_address' => 'IP Address',
+    'Activities' => 'Activities',
+    'url' => 'Url',
+    'causer' => 'Causer',
+    'subject_type' => 'Subject Type',
+    'subject_id' => 'Subject ID',
+    'when' => 'When'
+];
---
+++ lang/fa/filament/activities_page.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'ip_address' => 'آدرس IP',
+    'Activities' => 'فعالیت‌ها',
+    'url' => 'آدرس اینترنتی',
+    'causer' => 'عامل',
+    'subject_type' => 'نوع موضوع',
+    'subject_id' => 'شناسه موضوع',
+    'when' => 'زمان'
+];
+
