4193a909cb1906c62838caf56b6a44d05b75792d
norouzimohammadreza <rda0044@gmail.com>
2025-01-01 10:46:23 +0330

    Translate Relation post with tags

--- app/Filament/Resources/PostResource/RelationManagers/TagsRelationManager.php
+++ app/Filament/Resources/PostResource/RelationManagers/TagsRelationManager.php
@@ -2,11 +2,13 @@
 
 namespace App\Filament\Resources\PostResource\RelationManagers;
 
+use App\Filament\Resources\TagResource;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Illuminate\Contracts\Support\Htmlable;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
@@ -14,37 +16,21 @@ class TagsRelationManager extends RelationManager
 {
     protected static string $relationship = 'tags';
 
+    public static function getTitle(\Illuminate\Database\Eloquent\Model $ownerRecord, string $pageClass): string
+    {
+        return __('filament/dashboard.tags');
+    }
+
+
     public function form(Form $form): Form
     {
-        return $form
-            ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->required()
-                    ->maxLength(255),
-            ]);
+        return TagResource::form($form);
     }
 
     public function table(Table $table): Table
     {
-        return $table
-            ->recordTitleAttribute('name')
-            ->columns([
-                Tables\Columns\TextColumn::make('name'),
-            ])
-            ->filters([
-                //
-            ])
-            ->headerActions([
-                Tables\Actions\CreateAction::make(),
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
+        return TagResource::table($table)->headerActions([
+            Tables\Actions\CreateAction::make()->label(__('filament/tag.new_tag'))
+        ]);
     }
 }
