0333234f53a3d86dc2f60a07a8e8ac500371a475
norouzimohammadreza <rda0044@gmail.com>
2025-01-04 11:10:31 +0330

    Adding Show search queries value and show level log

--- app/Filament/Resources/LogResource.php
+++ app/Filament/Resources/LogResource.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources;
 
 use App\ActivityLogsFunctions\ActivityLogHelper;
+use App\Enums\LogLevelEnum;
 use App\Filament\Resources\LogResource\Pages;
 use Filament\Forms\Components\KeyValue;
 use Filament\Forms\Components\TextInput;
@@ -11,23 +12,26 @@ use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Actions\Action;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\Filter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Spatie\Activitylog\Models\Activity;
-use Filament\Tables\Filters\Filter;
 
 class LogResource extends Resource
 {
     protected static ?string $model = Activity::class;
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
     public static function getNavigationGroup(): ?string
     {
         return __('filament\dashboard.log_settings');
     }
+
     public static function getNavigationLabel(): string
     {
         return trans('filament\dashboard.activities');
     }
+
     public static function getPluralModelLabel(): string
     {
         return __('filament\activities_page.activities');
@@ -54,9 +58,22 @@ class LogResource extends Resource
                 TextColumn::make('causer.name')->label(__('filament\activities_page.causer')),
                 TextColumn::make('subject_id')->wrap()->label(__('filament\activities_page.subject_id')),
                 TextColumn::make('subject_type')->label(__('filament\activities_page.subject_type')),
+                TextColumn::make('level')->label(__('filament\model_record_log_setting.level'))
+                    ->getStateUsing(function (Activity $record){
+                        switch ($record->level){
+                            case  LogLevelEnum::LOW->value:
+                                return LogLevelEnum::LOW->translation();
+                            case  LogLevelEnum::MEDIUM->value:
+                                return LogLevelEnum::MEDIUM->translation();
+                            case  LogLevelEnum::HIGH->value:
+                                return LogLevelEnum::HIGH->translation();
+                            case  LogLevelEnum::CRITICAL->value:
+                                return LogLevelEnum::CRITICAL->translation();
+                        }
+                    }),
                 TextColumn::make('created_at')->label(__('filament\activities_page.when'))
                     ->sortable()->getStateUsing(function (Activity $activity) {
-                        return verta($activity->created_at,'Asia/Tehran');
+                        return verta($activity->created_at, 'Asia/Tehran');
                     })
                 ,
             ])->defaultSort('created_at', 'desc')
@@ -67,19 +84,21 @@ class LogResource extends Resource
                         return ActivityLogHelper::getInstance()->getViewUrl($record);
                     })
                     ->icon('heroicon-o-eye')
-                ->disabled(fn(Activity $record) => $record->event == 'HTTP SEARCH'),
+                    ->disabled(fn(Activity $record) => $record->event == 'HTTP SEARCH'),
                 Tables\Actions\ViewAction::make('detail')
                     ->label(__('filament\activities_page.details'))
                     ->form([
-                   KeyValue::make('properties.old'),
-                   KeyValue::make('properties.attributes'),
-                ])->disabled(fn(Activity $record) => $record->subject_id == null),
+                        KeyValue::make('properties.old'),
+                        KeyValue::make('properties.attributes'),
+                        TextInput::make('properties.resource'),
+                        TextInput::make('properties.search'),
+                    ]),
             ])
             ->filters([
                 Filter::make(__('filament\activities_page.event_subject'))
-                    ->query(fn(Builder $query) => $query->where('subject_id','!=',null)),
+                    ->query(fn(Builder $query) => $query->where('subject_id', '!=', null)),
                 Filter::make(__('filament\activities_page.log_search'))
-                    ->query(fn(Builder $query) => $query->where('log_name','HTTP SEARCH')),
+                    ->query(fn(Builder $query) => $query->where('log_name', 'HTTP SEARCH')),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
