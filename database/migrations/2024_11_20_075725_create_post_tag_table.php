face3ee8056894fe023c7a8b6707479fc3cd4053
norouzimohammadreza <rda0044@gmail.com>
2025-01-07 14:02:57 +0330

    Define a primary id for pivot model post_tag

--- database/migrations/2024_11_20_075725_create_post_tag_table.php
+++ database/migrations/2024_11_20_075725_create_post_tag_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('post_tag', function (Blueprint $table) {
+            $table->id();
             $table->unsignedBigInteger('post_id')->index();
             $table->unsignedBigInteger('tag_id')->index();
-            $table->primary(['post_id', 'tag_id']);
             $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
             $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
             $table->timestamps();
