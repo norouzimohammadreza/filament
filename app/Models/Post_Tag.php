c5381cb231163d544a6496ade584375fa6ceb992
norouzimohammadreza <rda0044@gmail.com>
2025-01-07 15:00:51 +0330

    Clean and optimize code

--- app/Models/Post_Tag.php
+++ app/Models/Post_Tag.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use App\ActivityLogsFunctions\Traits\MyLogActivityTrait;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Models\Activity;
@@ -15,14 +14,18 @@ final class Post_Tag extends Pivot
 
     public $incrementing = true;
     protected $table = 'post_tag';
-    protected $fillable = ['post_id', 'tag_id'];
+    protected $fillable = [
+        'post_id',
+        'tag_id',
+    ];
 
     public function tapActivity(Activity $activity, string $eventName)
     {
         $activity->level = 1;
         $activity->url = request()->getPathInfo();
-        $activity->ip= inet_pton(request()->ip());
+        $activity->ip = inet_pton(request()->ip());
     }
+
     public function getActivitylogOptions(): LogOptions
     {
         return LogOptions::defaults()
