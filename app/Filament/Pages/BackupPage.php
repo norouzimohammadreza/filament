999ba7baf361d68d482f69decbeaca50b678bd0e
norouzimohammadreza <rda0044@gmail.com>
2025-01-23 10:31:26 +0330

    Show any backup from project

--- app/Filament/Pages/BackupPage.php
+++ app/Filament/Pages/BackupPage.php
@@ -5,21 +5,37 @@ namespace App\Filament\Pages;
 use App\Models\BackupRecord;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Pages\Page;
+use Filament\Tables\Actions\CreateAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Table;
 
-class BackupPage extends Page
+class BackupPage extends Page implements HasTable
 {
     use InteractsWithTable;
     use InteractsWithForms;
     protected static ?string $navigationIcon = 'heroicon-m-x-mark';
 
     protected static string $view = 'filament.pages.backup-page';
-
     public function table(Table $table): Table
     {
-        return $table
-            ->query(BackupRecord::query())
-            ->columns([]);
+        return $table->query(BackupRecord::query())
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('path'),
+                TextColumn::make('size'),
+                TextColumn::make('is_file'),
+                TextColumn::make('is_database_record'),
+                TextColumn::make('created_at'),
+            ])
+            ->headerActions([
+                CreateAction::make()
+            ])
+            ->actions([
+                EditAction::make()
+            ]);
     }
+
 }
