426128e22a0b618944d8eec062307b200533e4e8
norouzimohammadreza <rda0044@gmail.com>
2025-01-25 14:18:18 +0330

    Translate backup files for en

--- lang/vendor/backup/en/backup.php
+++ lang/vendor/backup/en/backup.php
@@ -1,5 +1,13 @@
 <?php
 return [
     'backup' => 'Backup',
-    ''
+    'backup both' => 'Backup Both',
+    'Backup Database' => 'Backup Database',
+    'backup files' => 'Backup Files',
+    'name' => 'Name',
+    'path' => 'Path',
+    'size' => 'Size',
+    'type' => 'Type',
+    'time' => 'Time',
+    'download' => 'Download',
 ];
