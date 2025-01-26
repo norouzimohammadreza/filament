f25d79c1f27ee12deafa3b9299145ea52e54a895
norouzimohammadreza <rda0044@gmail.com>
2025-01-26 13:26:24 +0330

    Edit a wrong in translate files for backup

--- lang/en/filament/backup.php
+++ lang/en/filament/backup.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'backup' => 'Backup',
-    'backup both' => 'Backup Both',
-    'Backup Database' => 'Backup Database',
-    'backup files' => 'Backup Files',
+    'backup_both' => 'Backup Both',
+    'backup_database' => 'Backup Database',
+    'backup_files' => 'Backup Files',
     'name' => 'Name',
     'disk' => 'Disk',
     'path' => 'Path',
