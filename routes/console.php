ee55820c13b712c59b2cc725155727126466a0ca
norouzimohammadreza <rda0044@gmail.com>
2025-01-30 13:36:41 +0330

    Test Cron for schedule

--- routes/console.php
+++ routes/console.php
@@ -2,9 +2,20 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('backup:run --only-db')->everyMinute();
+$taskSchedules = \App\Models\Schedule::all();
+foreach ($taskSchedules as $key => $taskSchedule) {
+    $params = explode(",", $taskSchedule->params);
+    Schedule::call(function () use ($taskSchedule) {
+        \App\Jobs\FileBackup::dispatch();
+        Log::info("{$taskSchedule->name} run at " . now());
+    })->{$taskSchedule->frequency}(...$params)->name($taskSchedule->name);
+    Schedule::call(function () use ($taskSchedule) {})->everyMinute();
+    Schedule::call(function () use ($taskSchedule) {})->cron("* * * * *");
+}
+//Schedule::command('backup:run --only-db')->everyMinute();
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
